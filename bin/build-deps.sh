cbadd7ab776662d5b5b06c592587cb468253f6b6
W. Watson <pair@cnfdev03.vulk.coop>
2019-12-12 23:07:48 +0000

    added shards to path for build

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -4,9 +4,10 @@ echo "installing crystal version 0.31.1..."
 apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl
 wget -O crystal.tgz https://github.com/crystal-lang/crystal/releases/download/0.31.1/crystal-0.31.1-1-linux-x86_64.tar.gz
 tar -C /usr/local -xzf crystal.tgz
-cd /usr/local/crystal-0.31.1-1/bin/
+# cd /usr/local/crystal-0.31.1-1/bin/
 pwd
 ls -al
 
 ln -s /usr/local/crystal-0.31.1-1/bin/crystal /usr/local/bin/crystal
+export PATH=/usr/bin/shards:$PATH
 # export PATH=/usr/local/bin:$PATH
