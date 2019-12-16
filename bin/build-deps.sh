a4f3d2c261fa2716b4e3bdf242b9c4ce14139e92
denverwilliams <denver@debian.nz>
2019-12-16 22:48:17 +0000

    Disable build-deps for github-actions

--- bin/build-deps.sh
+++ bin/build-deps.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "installing crystal version 0.31.1..."
-apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl
-wget -O crystal.tgz https://github.com/crystal-lang/crystal/releases/download/0.31.1/crystal-0.31.1-1-linux-x86_64.tar.gz
-tar -C /usr/local -xzf crystal.tgz
+#echo "installing crystal version 0.31.1..."
+#apk add --no-cache --virtual .build-deps bash gcc musl-dev openssl
+#wget -O crystal.tgz https://github.com/crystal-lang/crystal/releases/download/0.31.1/crystal-0.31.1-1-linux-x86_64.tar.gz
+#tar -C /usr/local -xzf crystal.tgz
 # cd /usr/local/crystal-0.31.1-1/bin/
-pwd
-ls -al
+#pwd
+#ls -al
 
-ln -s /usr/local/crystal-0.31.1-1/bin/crystal /usr/local/bin/crystal
-export PATH=/usr/bin/shards:$PATH
+#ln -s /usr/local/crystal-0.31.1-1/bin/crystal /usr/local/bin/crystal
+#which shards
+#export PATH=/usr/bin/shards:$PATH
 # export PATH=/usr/local/bin:$PATH
